d4a6f3c65fe8f8aea8bac37bb95ce8ca60a9a899
danil <daniiltrotsenko2002@gmail.com>
2024-09-28 20:19:35 +0300

    1

--- game.py
+++ game.py
@@ -6,14 +6,22 @@ class Game():
         self.width = 1600
         self.height = 900
 
-        self.screen = pygame.display.set_mode()
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.fps = 60
         self.clock = pygame.time.Clock()
 
         self.running = True
 
-        self.tower = Tower("tower.png")
+        # self.tower = Tower("tower.png")
 
-        def main_loop(self):
-            if self.running:
-                pass
+    def main_loop(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill('green')
+
+            pygame.display.update()
+            self.clock.tick(self.fps)
+
